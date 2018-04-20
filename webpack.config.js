7696ce60b1e6f9d331874b9dc1efc3727026b9f2
Daeseok Youn <daeseok.youn@gmail.com>
2018-04-20 01:06:26 +0000

    Initial commit for reactjs

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// load webpack -> es6 에서는 import
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        hot: true,
+        inline: true,
+        host: '0.0.0.0',
+        port: 8080,
+        contentBase: __dirname + '/public/',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
