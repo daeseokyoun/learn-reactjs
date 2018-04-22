ea36b923043f72bbd6cbb928e5cc0f0e5b757d82
Daeseok Youn <daeseok.youn@gmail.com>
2018-04-22 04:39:08 +0000

    Add simple search function with ContactInfo class
    Youtube lecture (korean)
    https://www.youtube.com/watch?v=LSTVOuyF6zE&index=12&list=PL9FpF_z-xR_GMujql3S_XGV2SpdfDBkeC&t=0s

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Contact from './Contact'
  
 class App extends React.Component {
     constructor(props) {
@@ -10,10 +11,7 @@ class App extends React.Component {
     render(){
  
         return (
-            <div>
-                <button onClick={()=> {this.setState({name: 'daeseok'});}}>Click</button>
-                <h1>Hello!! {this.state.name}</h1>
-            </div>
+            <Contact />
         );
     }
 }
